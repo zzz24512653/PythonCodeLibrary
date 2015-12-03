046dda3ebb6ddcd2b0fcb0edd52155bd7a4588a1
zzz24512653 <zzz24512653@gmail.com>
2015-12-03 10:07:46 +0800

    update

--- sort/QuickSort.py
+++ sort/QuickSort.py
@@ -17,7 +17,7 @@ def quick_sort(nums, reverse=False):
     :param reverse: False means sort from small to big, otherwise is sort form big to small
     :return: nums after sorted
     """
-    if nums is None or len(nums) == 0:
+    if nums is None or len(nums) < 2:
         return nums
     if reverse is True:
         partition(nums, 0, len(nums)-1, lambda a, b: a > b)
--- sort/Tester.py
+++ sort/Tester.py
@@ -1,16 +1,22 @@
 from QuickSort import quick_sort
+from MergeSort import merge_sort
+from BubbleSort import bubble_sort
+from InsertSort import insert_sort
 
 
 def test(sort_fn, reverse=False):
     print "sort algorithm:", sort_fn.__name__
-    print "doc:", quick_sort.__doc__
+    print "doc:", sort_fn.__doc__
     print
-    test_cases = [None, [], [1], [1, 2, 3], [3, 2, 1], [5, 7, 1, 3, -2]]
+    test_cases = [None, [], [1], [1, 2, 3], [3, 2, 1], [5, 7, 1, 3, -2], [5, 1, 7, -2, 1, 3, -2]]
     for case in test_cases:
         print "test case:", case
         print "after sort:", sort_fn(case, reverse)
         print
 
 if __name__ == "__main__":
-    test(quick_sort, True)
-
+    # test(quick_sort, True)
+    # test(merge_sort, True)
+    # test(bubble_sort, False)
+    test(insert_sort, True)
+    pass
